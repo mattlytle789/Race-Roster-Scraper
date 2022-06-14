4b0de524b3567892fcf600f652bf7ce1acd3d00e
Matthew Lytle <mbl31@pitt.edu>
2022-06-14 12:30:59 -0400

    Added html parsing functionality

---
+++ HTMLTesting.py
@@ -0,0 +1,76 @@
+import requests
+from bs4 import BeautifulSoup
+import xlwt
+from xlwt import Workbook
+
+#class MyHTMLParser(HTMLParser):
+#	# Variables for tjhe parser
+#	self.currStartTag = "" # holds the current start tags delimiter
+#	self.currData = "" # holds the current tags data
+#	self.individualTagFlag = false # flag to mark when the individuals tag is found
+	
+#	def handle_starttag(self, tag, attrs):
+#		# Storing the current tags delimiter
+#		self.currStartTag = tag
+
+#	def handle_data(self, data):
+#		# Storing the current tags delimiter
+#		self.currData = data
+#		# checking if this is the Individuals tag
+#		if (self.currStartTag == 'h2' & self.currData == 'Individuals'):
+#			self.individualTagFlag = true
+
+# Accessing the data from the website
+url = 'https://raceroster.com/events/2022/56070/the-2022-asics-falmouth-road-race/fundraising-organization/34911'
+response = requests.get(url) # variable to hold the response to the request in a string
+#print(response.text) ####DEBUGGING
+
+# Creating the soup object for the html parser
+htmlSoup = BeautifulSoup(response.text, 'html.parser')
+
+# lists to hold the participants and their earnings
+participantData = []
+# ResultSet that hold all of the paragraph tags
+pTags = htmlSoup.find_all('p')
+
+# Filling the participants data into a list
+individualsStartFlag = False # flag to signal the start of the list of individuals
+for tag in pTags:
+	#print(tag) ####DEBUGGING
+	if (individualsStartFlag):
+		participantData.append(tag.text)
+	if (tag.text == 'Click on an individual below to make a donation.'):
+		individualsStartFlag = True
+	if (tag.text == ''):
+		individualsStartFlag = False
+
+# Splitting the participants and earnings 
+index = 0 # variable to track what the index of he list is
+names = [] # lsit of the participants names
+earnings = [] # list of the participants earnings
+for data in participantData:
+	if (index%2 == 0): # if the index is a name cell
+		if (data != ''):
+			names.append(data)
+	else: # if the index is an earnings cell
+		if (data != ''):
+			earnings.append(data[9:])
+	index+=1
+
+# writing data to an excel file
+excelFile = Workbook()
+sheet1 = excelFile.add_sheet('Sheet1')
+for i in range(0,len(names)):
+	sheet1.write(i,0,names[i])
+	sheet1.write(i,1,earnings[i])
+excelFile.save('RaceRosterData.xls')
+# creating a dictionary with the participants and earnings
+#participantDict = {}
+#for i in range(0,len(names)):
+#	participantDict[names[i]] = int(earnings[i])
+
+####DEBUGGING
+#print(participantData)
+#print(names)
+#print(earnings)
+#print(participantDict)
